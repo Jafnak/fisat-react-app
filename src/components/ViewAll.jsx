ea3db08428f92764e7a922ba51b6b9e4dae4ffd4
Jafnak <jafnakusman@gmail.com>
2024-06-09 13:48:00 +0530

    ViewAll  component added

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const ViewAll = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">First name</th>
+      <th scope="col">Last name</th>
+      <th scope="col">College</th>
+      <th scope="col">DOB</th>
+      <th scope="col">Course</th>
+      <th scope="col">Mobile</th>
+      <th scope="col">Email</th>
+      <th scope="col">Address</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row">1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default ViewAll
